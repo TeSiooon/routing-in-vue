38113cce957ba657d2f06933a3234b25657133f7
TeSiooon <piotr.kowalczyk9908@gmail.com>
2024-05-24 13:26:56 +0200

    routing setup and rendering firsts routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
 
-const app = createApp(App)
+const router = createRouter({
+  // Sledzenie historii na stronie np jesli klikniesz back to
+  // cofnie tam gdzie tego oczekujemyt
+  history: createWebHistory(),
+  routes: [
+    // dany komponent jest ladowany na danej sciezce
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ],
+});
+
+const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
