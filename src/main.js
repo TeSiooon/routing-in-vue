780b918b037f0108c2c0a5e35435fe3c9eedc8c7
TeSiooon <piotr.kowalczyk9908@gmail.com>
2024-05-27 10:47:15 +0200

    redirecting and ctach all routes

--- src/main.js
+++ src/main.js
@@ -5,18 +5,25 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMebers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   // Sledzenie historii na stronie np jesli klikniesz back to
   // cofnie tam gdzie tego oczekujemyt
   history: createWebHistory(),
   routes: [
+    //  Redirect to wiadomka przekierowanie ale mozna zrobic to inaczej ('lepiej')
+    // I tutaj chodzi o alias
+    //  { path: '/teams', component: TeamsList, alias: '/' },
+    { path: '/', redirect: '/teams' },
     // dany komponent jest ladowany na danej sciezce
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
     // Definicja dynamicznej sciezki
     // Jesli damy props router wie ze dinamiczne sciezki sa przekazywane przez propsy
     { path: '/teams/:teamId', component: TeamMebers, props: true },
+    // Dynamiczne przekierowanie uzytkownika jesli cos zle wpisze
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
